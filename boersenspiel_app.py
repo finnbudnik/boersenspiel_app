dc388d970719f172c17be8c337352810c92ce4bc
finnbudnik <finn.budnik@web.de>
2025-05-06 08:46:22 +0200

    Add files via upload

--- boersenspiel_app.py
+++ boersenspiel_app.py
@@ -162,7 +162,7 @@ def landing_page():
             lunaris = next((s for s in stocks if s.name == "Lunaris Ventures"), None)
             assert lunaris is not None, "Lunaris Ventures wurde nicht in stocks gefunden!"
             amount = 10
-            buy_price = lunaris.price_history[0]  # Preis in Periode 1
+            buy_price = round(lunaris.price_history[0], 2)  # Preis in Periode 1
             player.portfolio["Lunaris Ventures"] = {"amount": amount, "buy_price": buy_price}
 
 
